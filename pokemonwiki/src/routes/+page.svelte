f4feaaf522b5c0fa83596a1ab90628c7f7b5687b
JIHWAN <jjjh0508@gmail.com>
2023-08-13 01:16:15 +0900

    first commit

---
+++ pokemonwiki/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+    <title>포켓몬 위키</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 font-bold underline">
+   포켓몬 위키
+</h1>
+
---
+++ pokemonwiki/src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<h2 class="text-4xl text-center">
+    포켓몬 위키 입니다.</h2>
+<h3 class="text-2xl text-center">made by ji</h3>
